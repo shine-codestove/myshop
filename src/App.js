bb86c7345493dd83ad8b62cc660ca048dde3afad
shine-codestove <shine@codestove.io>
2022-06-29 10:37:18 +0900

    edit comp

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
-import ProductList from "./pages/product/ProductList";
+import HelloComponent from "./components/HelloComponent";
 
 function App() {
     return (
         <div className="App">
             {/*<ProductList />*/}
             <h1>내가 만든 리액트</h1>
+            <HelloComponent />
         </div>
     );
 }
